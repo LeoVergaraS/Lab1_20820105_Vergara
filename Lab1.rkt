beee74c0db690df3464a95580c1ab064aaafe747
Leo Vergara <leo.vergara@usach.cl>
2021-10-17 16:50:40 -0300

    TDA fecha y TDA plataforma->DocsDuck

--- Lab1.rkt
+++ Lab1.rkt
@@ -1,1 +1,37 @@
 #lang racket
+; TDA FECHA:
+; Constructor tda de fecha.
+; dominio: 3 enteros positivos + {0}
+; recorrido: Una lista
+(define fecha(lambda (dia mes anio) (list dia mes anio)))
+; Selectores tda fecha.
+(define getDia car)
+(define getMes(lambda(lista)
+                (car(cdr lista))))
+(define getAnio(lambda(lista)
+                 (car(cdr(cdr lista)))))
+; Modificadores tda lista
+(define setDia(lambda(lista dia)
+                (list dia (getMes lista) (getAnio lista))))
+(define setMes(lambda(lista mes)
+                (list (getDia lista) mes (getAnio lista))))
+(define setAnio(lambda(lista anio)
+                 (list (getDia lista) (getMes lista) anio)))
+
+
+; TDA DocsDuck
+; Constructor de DocsDuck
+(define DocsDuck(lambda (nombre fecha funcionEncriptado funcionDesencriptado)
+                  (list nombre fecha funcionEncriptado funcionDesencriptado)))
+; Selectores
+(define getNombre car)
+(define getFecha(lambda(lista)
+                  (car(cdr lista))))
+(define getFE(lambda(lista)
+                  (car(cdr(cdr lista)))))
+(define getFD(lambda(lista)
+                  (car(cdr(cdr(cdr lista))))))
+
+
+
+
